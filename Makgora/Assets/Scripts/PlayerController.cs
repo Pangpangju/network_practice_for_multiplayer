d2861920a242b7ed07b62ccdf594e331d50b4795
Pangpangju <joo990319@naver.com>
2021-11-29 08:37:23 +0900

    1.0

---
+++ Makgora/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public void FixedUpdate()
+    {
+        SendInputToServer();
+    }
+    private void SendInputToServer() {
+        bool[] _inputs = new bool[] {
+            Input.GetKey(KeyCode.W),
+            Input.GetKey(KeyCode.S),
+            Input.GetKey(KeyCode.A),
+            Input.GetKey(KeyCode.D),
+        };
+        ClientSend.PlayerMovement(_inputs);
+    }
+}
---
+++ MakgoraServer/MakgoraServer/GameLogic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MakgoraServer
+{
+    class GameLogic
+    {
+        public static void Update() {
+            foreach (Client _client in Server.clients.Values) {
+                if (_client.player != null) {
+                    _client.player.Update();
+                }
+            }
+            ThreadManager.UpdateMain();
+        }
+    }
+}
